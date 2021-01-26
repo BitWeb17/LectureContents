50a5141f806a1690f9278e19ba2b36fa26bdb076
silenc3502 <gcccompil3r@gmail.com>
2021-01-26 15:56:10 +0900

    Twentieth Comprehensive Exercise

--- GitAssociated/src/Nineteenth/ItemEnhanceThreadTest.java
+++ GitAssociated/src/Nineteenth/ItemEnhanceThreadTest.java
@@ -69,9 +69,17 @@ public class ItemEnhanceThreadTest {
     // Mutex와 Semaphore의 차이점이 무엇인가 ?
     // 대기열이 있다 없다.
     public static void main(String[] args) {
+        // 현재 자기 자신에 해당하는 객체 iet를 생성한다.
         final ItemEnhanceThreadTest iet = new ItemEnhanceThreadTest();
 
+        // 루프를 6번 돌기 때문에 6개의 스레드가 생성된다.
         for(int i = 0; i < 6; i++) {
+            // 아래 보이는 형식의 패턴이 바로 익명 객체에 해당한다.
+            // 익명 객체를 사용하는 이유는 무엇일까 ?
+            // 기본적으로 new를 한다는 것은 이 정보를 저장할 변수가 필요하다.
+            // 문제는 이런 변수를 저장하게 되면
+            // 자바의 GC(Garbage Collector)가 이것을 함께 관리해야한다.
+            // 즉 일회성의 목적을 가지고 익명 객체를 생성하게 된다.
             Thread t = new Thread(new Runnable() {
                 @Override
                 public void run() {
