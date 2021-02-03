0ba52a0121c55f3aad9540e1eb5e0a3532d71bac
silenc3502 <gcccompil3r@gmail.com>
2021-02-03 12:37:03 +0900

    JS Team Lotto Intermediate

--- JavaScriptLecture/js_arrow/src/exercise/Prob.js
+++ JavaScriptLecture/js_arrow/src/exercise/Prob.js
@@ -59,6 +59,8 @@ const CreateRandomTeam = (arr, num) => {
 
 const CheckWinner = (arr, winArr) => {
     const CRITERIA = 1013
+    // const CRITERIA = 241
+    // const CRITERIA = 2
 
     for(var i = 0; i < arr.length; i++) {
         if(!(arr[i] % CRITERIA)) {
@@ -69,8 +71,107 @@ const CheckWinner = (arr, winArr) => {
     }
 }
 
-const CalcDividend = () => {
+Array.prototype.arrDivide = function (num) {
+    let arr = this
+    let len = arr.length
+    // 딱 떨어지는 경우엔 floor OK
+    // 나머지가 존재할 경우 ceil 올림으로 처리하면 될 듯 ?
+    let cnt = Math.ceil(len / num)
 
+    let newArr = []
+
+    for(var i = 0; i < cnt; i++) {
+        newArr.push(arr.splice(0, num))
+    }
+
+    return newArr
+}
+
+Array.prototype.printArr = function(num) {
+    let arr = this
+    let len = arr.length
+
+    for (var i = 0; i < len; i += num) {
+        console.log(arr[i], arr[i + 1], arr[i + 2], arr[i + 3])
+    }
+}
+
+const CalcDividend = (teamArr, winArr, price) => {
+    const ALL = 16
+    const THREEFOUR = 8
+    const TWOFOUR = 4
+    const ZERO = 0
+
+    const TEAMMEMNUM = 4
+
+    // filter, every
+    let divWinArr = winArr.arrDivide(TEAMMEMNUM)
+
+    let teamNum = divWinArr.length
+    let allClearRateArr = new Array(teamNum)
+    let subClearRateArr = new Array(teamNum)
+
+    for(var i = ZERO; i < teamNum; i++) {
+        allClearRateArr[i] = divWinArr[i].every(allClear)
+        subClearRateArr[i] = divWinArr[i].reduce(subClear)
+    }
+
+    function allClear(value) {
+        return value > ZERO
+    }
+
+    function subClear(total, value) {
+        return total + value
+    }
+
+    console.log('All Clear Team: ' + allClearRateArr)
+    console.log('Sub Clear Team: ' + subClearRateArr)
+
+    // For Debugging
+    //divWinArr.printArr(TEAMMEMNUM)
+
+    //console.log("divWinArr = " + divWinArr)
+
+    // 현재까지 체크된 것이 배당금 16배수인 팀을 확보함
+    // 배당금 8배, 4배, 1배인 팀도 확보한 상태
+    // 전체 상금을 몇 등분할지에 대한 계산이 필요하다.
+    let dividendRate = allClearRateArr.reduce(checkTrueNum)
+
+    function checkTrueNum(total, value) {
+        if(value == true) {
+            return total + ALL
+        } else {
+            return total
+        }
+    }
+
+    console.log("All Clear Dividend Rate = " + dividendRate)
+
+    dividendRate += subClearRateArr.reduce(allSum)
+
+    function allSum(total, value) {
+        return total + value
+    }
+
+    console.log("Total Dividend Rate = " + dividendRate)
+
+    let dividend = Math.round(price / dividendRate)
+
+    console.log("1배수 배당금은 " + dividend + "입니다.")
+    console.log("인당 배당금은 " +
+        Math.round(dividend / TEAMMEMNUM) + "입니다.")
+
+    console.log("4배수 배당금은 " + TWOFOUR * dividend + "입니다.")
+    console.log("인당 배당금은 " +
+        Math.round(TWOFOUR * dividend / TEAMMEMNUM) + "입니다.")
+
+    console.log("8배수 배당금은 " + THREEFOUR * dividend + "입니다.")
+    console.log("인당 배당금은 " +
+        Math.round(THREEFOUR * dividend / TEAMMEMNUM) + "입니다.")
+
+    console.log("16배수 배당금은 " + ALL * dividend + "입니다.")
+    console.log("인당 배당금은 " +
+        Math.round(ALL * dividend / TEAMMEMNUM) + "입니다.")
 }
 
 const Prob = () => {
@@ -94,6 +195,10 @@ const Prob = () => {
 
     CheckWinner(arr, WinnerArr)
     console.log("After CheckWinner() = " + WinnerArr)
+
+    CalcDividend(teamArr, WinnerArr, PRICE)
+    console.log("팀 로또가 성공적으로 마무리 되었습니다.")
+    console.log("다음에도 잘 부탁드립니다 호갱님 ^^")
 
     return (
         <div className="Prob">
