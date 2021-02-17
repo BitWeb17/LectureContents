6dcae86c994c9f8e39b8fb93f44cbb68dd6cd24d
silenc3502 <gcccompil3r@gmail.com>
2021-02-17 13:18:53 +0900

    Second Spring Annotation Examples

---
+++ SpringFrameworkLecture/demo/src/main/java/com/example/demo/controller/thymeleaf/ThymeLeafTestController.java
@@ -0,0 +1,38 @@
+package com.example.demo.controller.thymeleaf;
+
+import com.example.demo.controller.request.RequestTestController;
+import com.example.demo.entity.TestMember;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/thymeleaf")
+public class ThymeLeafTestController {
+    private static final Logger log =
+            LoggerFactory.getLogger(ThymeLeafTestController.class);
+
+    // 'TestMember' available as request attribute
+    // 속성 설정이 안되어 있다는 에러 메시지가 나타난다.
+    @GetMapping("/registerForm001")
+    public String registerForm001() {
+        log.info("registerForm001()");
+
+        return "spring/thymeleaf/registerForm";
+    }
+
+    @GetMapping("/registerForm002")
+    public String registerForm002(Model model) {
+        log.info("registerForm002()");
+
+        /* 모델은 데이터를 제어하는 객체라고 설명했었음
+           스프링에서 활용할 수 있는 데이터 속성 이름으로 TestMember를 만들었음
+           또한 이 TestMember 속성은 new TestMember()에 의해 생성된 객체를 가짐 */
+        model.addAttribute("TestMember", new TestMember());
+
+        return "spring/thymeleaf/registerForm";
+    }
+}
