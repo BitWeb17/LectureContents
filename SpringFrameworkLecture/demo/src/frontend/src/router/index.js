6f8f5389ecc576228dd9a432ac886ea6d173eabe
silenc3502 <gcccompil3r@gmail.com>
2021-02-23 12:22:37 +0900

    Vue Third

---
+++ SpringFrameworkLecture/demo/src/frontend/src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import About from '../views/About'
+import Test from '../views/Test'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About
+  },
+  {
+    path: '/test',
+    name: 'Test',
+    component: Test
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
---
+++ SpringFrameworkLecture/demo/src/frontend/src/views/Home.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="home">
+    <div id="header">
+      <router-link :to="{ name: 'Home' }"
+                   class="nav-link" active-class="active">
+        Home
+      </router-link>
+      <router-link :to="{ name: 'About' }"
+                   class="nav-link" active-class="active">
+        About Us
+      </router-link>
+      <router-link :to="{ name: 'Test' }"
+                   class="nav-link" active-class="active">
+        Test
+      </router-link>
+    </div>
+    <h1>페이지 홈(Home)</h1>
+    <div id="app">
+      {{ message }}<br>
+    </div>
+  </div>
+</template>
+
+<script>
+
+/* eslint-disable no-unused-vars */
+import store from '../store'
+import Vue from 'vue'
+import cookies from 'vue-cookies'
+
+Vue.use(cookies)
+
+export default {
+  name: 'Home',
+  data: function () {
+    return {
+      message: 'test'
+    }
+  }
+}
+</script>
+
+<style scoped>
+div {
+  border: 1px solid #cccccc;
+}
+#header {
+  padding: 15px;
+  margin-bottom: 15px;
+  margin: 5px 5px;
+}
+</style>
