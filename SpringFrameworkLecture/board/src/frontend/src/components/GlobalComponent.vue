bd69eb0fd7feb821d611d22e275f03bc73d6ccd1
silenc3502 <gcccompil3r@gmail.com>
2021-02-25 15:42:19 +0900

    Vue Fifth

---
+++ SpringFrameworkLecture/board/src/frontend/src/components/GlobalComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <button @click="addCounter">{{ counter }}</button>
+</template>
+
+<script>
+export default {
+  name: "GlobalComponent",
+  // GlobalComponent 연결시 initial-counter
+  props: ['initialCounter'],
+  data: function () {
+    return {
+      counter: this.initialCounter
+    }
+  },
+  methods: {
+    addCounter () {
+      this.counter++
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- SpringFrameworkLecture/board/src/frontend/src/views/Test.vue
+++ SpringFrameworkLecture/board/src/frontend/src/views/Test.vue
@@ -11,6 +11,14 @@
     <input type="button" @click="increment()" value="increment"/><br>
     <input type="button" @click="decrement()" value="decrement"/><br>
     <input type="button" @click="randomNumber()" value="random"/><br>
+
+    <!-- Component를 만든다는 것은 결국
+         나만의 커스텀 HTML 태그를 만든다는 것을 의미한다.
+         LocalComponent가 쓰는 num은
+         현재 Component(Test)에서 사용하는 value와 같다. -->
+    <local-component :num="value"></local-component>
+    <button v-on:click="plus">클릭</button><br>
+    <global-component :initial-counter="counter"></global-component>
   </div>
 </template>
 
@@ -18,14 +26,23 @@
 import Vue from 'vue'
 import cookies from 'vue-cookies'
 
+import LocalComponent from "@/components/LocalComponent";
+import GlobalComponent from "@/components/GlobalComponent";
+
+Vue.component(GlobalComponent.name, GlobalComponent)
 Vue.use(cookies)
 
 export default {
   name: "Test",
   data () {
     return {
-      message: "안녕 난 뷰야 ~"
+      message: "안녕 난 뷰야 ~",
+      counter: 7000,
+      value: 3000
     }
+  },
+  components: {
+    'local-component': LocalComponent
   },
   methods: {
     reverseMsg: function () {
@@ -43,6 +60,9 @@ export default {
     },
     randomNumber () {
       this.$store.dispatch('generateRandomNumber')
+    },
+    plus () {
+      this.value++
     }
   },
   created() {
